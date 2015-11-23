c2da627fa5f2b73ffcc842f0b2cb7bc4631aab04
simplesthing <i.am.simplesthing@gmail.com>
2015-11-22 17:01:11 -0800

    adding all full res images

---
+++ app/models/cards.js
@@ -0,0 +1,64 @@
+define(function(require){
+	var Cards = function(){
+		var model = {};
+
+		var init =  function(path, callback){
+			var httpRequest = new XMLHttpRequest();
+			httpRequest.onreadystatechange = function(){
+				if(this.readyState == 4) {
+					var data = JSON.parse(httpRequest.responseText);
+					if(callback) callback(data);
+				}
+			};
+			httpRequest.open('GET', path);
+			httpRequest.send();
+		};
+
+		model.handleCardClick = function(evt){
+			evt.preventDefault();
+			evt.stopPropagation();
+
+			console.log(event.target);
+		};
+
+		model.addCardsToDOM = function(container){
+			var _container,arcana, suit;
+			_container = document.querySelector('#'+container);
+
+			model.cards.forEach(function(card,i){
+				if(card.arcana !== arcana){
+					var h1 = document.createElement('h1');
+					arcana = card.arcana;
+					h1.innerHTML = card.arcana;
+					_container.appendChild(h1);
+				}
+				if(card.suit && card.suit !== suit){
+					var h2;
+					suit = card.suit;
+					h2 = document.createElement('h2');
+					h2.innerHTML = card.suit;
+					_container.appendChild(h2);
+				}
+				var img = document.createElement("img");
+				img.setAttribute('id', i);
+				img.setAttribute('alt', card.name);
+				img.src = 'images/tarot/'+card.suit+'/'+card.number+'.jpg';
+				img.width='100';
+				img.addEventListener('click', model.handleCardClick, false);
+				_container.appendChild(img);
+			});
+		};
+
+
+		
+		init('models/cards.json', function(data){
+			model.cards = data;
+			model.addCardsToDOM('cards');
+		});
+
+		return model;
+	};
+
+	Cards();
+
+});
