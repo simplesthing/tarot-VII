2264062503ece7e32dcddc9cc3086989e9ee7f55
simplesthing <i.am.simplesthing@gmail.com>
2015-11-23 18:44:29 -0800

    starting popup

--- app/modules/helpers/helpers.js
+++ app/modules/helpers/helpers.js
@@ -2,13 +2,12 @@
 
 define(function(require){
   return {
-    // http://www.kirupa.com/html5/get_element_position_using_javascript.htm
     getPosition: function(element){
       let xPosition = 0, yPosition = 0;
 
     while(element) {
-        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
-        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        xPosition += (element.offsetLeft + element.clientLeft);
+        yPosition += (element.offsetTop + element.clientTop);
         element = element.offsetParent;
     }
     return { x: xPosition, y: yPosition };
--- app/modules/popup/popup.js
+++ app/modules/popup/popup.js
@@ -4,30 +4,51 @@ define(function(require){
   var helper = require('../helpers/helpers');
   var model = {};
 
-  // console.log(helper)
+  function Popup(){
+    let outer = document.createElement('div');
+    outer.style.width = (window.innerWidth/2)+'px';
+    outer.classList.add('card-popup');
+
+    let inner = document.createElement('div');
+    inner.classList.add('card-popup__inner');
+
+    let pointer = document.createElement('span');
+    pointer.classList.add('card-popup__pointer');
+
+    outer.appendChild(inner);
+    outer.appendChild(pointer);
+
+    return outer;
+  }
 
   model.addPopUp = function(elem){
-    var el, elPos, div;
+    var el, elPos, popup, offsetLeft;
 
+    // get exact location of card x and y
     el = document.getElementById(elem);
     elPos = helper.getPosition(el);
-    div = document.createElement('div');
-    div.style.width ='100px';
-    div.style.height ='100px';
-    div.style.background ='#bacd11';
-    div.style.position ='absolute';
-    div.style.top = elPos.y +'px';
-    div.style.left = elPos.x + 'px';
-    div.style.zIndex = '10';
-    document.body.appendChild(div);
-    // div.setAttribute(id)
-    // get exact location of card x and y
-    // console.log();
-    // determine if element is on left or right side of screen
-    // attach a popup div to page
+
+    // determine if element is on left or right offsetLeft of screen
+    offsetLeft = (elPos.x >  window.innerWidth/2) -100 ? -window.innerWidth/2:  100 ;
+
+    popup = new Popup();
+    // popup = document.createElement('div');
+    popup.style.width = (window.innerWidth/2)+'px';
+    // popup.classList.add('card-popup');
+
+    popup.style.position ='absolute';
+    popup.style.top = elPos.y +'px';
+
+    popup.style.left = (elPos.x + offsetLeft) + 'px';
+
+    popup.style.zIndex = '10';
+    document.body.appendChild(popup);
 
 
-  };
+      // attach a popup div to page
 
-  return model;
-});
+
+    };
+
+    return model;
+  });
